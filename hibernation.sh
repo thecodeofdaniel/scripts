6c52809828540bcc7c232507ba5104266e54dfec
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2023-11-17 19:18:12 -0800

    Initial commit

---
+++ hibernation.sh
@@ -0,0 +1,30 @@
+#!/bin/env bash
+
+if [ $# -eq 0 ]; then
+    echo "Error: Enter size for swapfile"
+    exit 1
+fi
+
+# Grab swap size from passed in argument
+swapSize="$1"
+
+# Create/Activate swapfile and add to fstab file
+sudo fallocate -l ${swapSize}G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+# Grab UUID and offset of swapfile
+UUID=$(findmnt -no UUID -T /swapfile)
+offset=$(sudo filefrag -v /swapfile | awk '{ if($1=="0:"){print $4} }' | sed 's/\.$//' | sed 's/\.$//')
+
+# Kernel stub
+sudo kernelstub -a "resume=UUID=$UUID resume_offset=$offset"
+
+# Add following line to file
+mkdir -p /etc/initramfs-tools/conf.d
+echo "resume=UUID=$UUID resume_offset=$offset" > /etc/initramfs-tools/conf.d/resume
+
+# Update the configurations
+sudo update-initramfs -u

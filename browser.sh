4572776be96e5fc9be8f1472c07c54fe8b1c22f3
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-03-01 00:31:00 -0800

    Add comments and change function names

--- browser.sh
+++ browser.sh
@@ -1,53 +1,50 @@
 #!/bin/env bash
 
 # The name of the profile for firefox
-PROFILE_NAME="Daniel"
+PROFILE_NAME="DANIEL"
 
 # Firefox and Brave profiles
-FIREFOX="$HOME/.mozilla/firefox/$PROFILE_NAME"
-BRAVE="$HOME/.config/BraveSoftware/Brave-Browser/Default"
+FIREFOX_PROFILE_LOC="$HOME/.mozilla/firefox/$PROFILE_NAME"
+BRAVE_PROFILE_LOC="$HOME/.config/BraveSoftware/Brave-Browser/Default"
 
 
 # This will insert an existing profile to firefox
-function _insertion
+function _USBtoComputer
 {
-    counter=0
-
+    # check if Firefox profile on USB exists
     if [ -d "./$PROFILE_NAME-firefox" ]; then
-        cp "./$PROFILE_NAME-firefox" "$FIREFOX"
-        ((counter++))
+        cp "./$PROFILE_NAME-firefox" "$FIREFOX_PROFILE_LOC"
+        printf "Copied Firefox profile from USB to computer\n"
     fi
 
+    # check if Brave profile on USB exists
     if [ -d "./$PROFILE_NAME-brave" ]; then
-        cp "./$PROFILE_NAME-brave" "$BRAVE"
-        ((counter++))
+        cp "./$PROFILE_NAME-brave" "$BRAVE_PROFILE_LOC"
+        printf "Copied Brave profile from USB to computer\n"
     fi
-
-    printf "Inserted $counter profiles\n"
 }
 
 
-function _extraction
+function _computerToUSB
 {
-    counter=0
     cp "$0" "$DEST" # copy this script to destination
 
-    if [ -d "$FIREFOX" ]; then
-        cp -r "$FIREFOX" "$DEST/$PROFILE_NAME-firefox"
-        ((counter++))
+    # check if Firefox profile on computer exists
+    if [ -d "$FIREFOX_PROFILE_LOC" ]; then
+        cp -r "$FIREFOX_PROFILE_LOC" "$DEST/$PROFILE_NAME-firefox"
+        printf "Copied Firefox profile from Computer to USB\n"
     fi
 
-    if [ -d "$BRAVE" ]; then
-        cp -r "$BRAVE" "$DEST/$PROFILE_NAME-brave"
-        ((counter++))
+    # check if Brave profile on computer exists
+    if [ -d "$BRAVE_PROFILE_LOC" ]; then
+        cp -r "$BRAVE_PROFILE_LOC" "$DEST/$PROFILE_NAME-brave"
+        printf "Copied Brave profile from Computer to USB\n"
     fi
-
-    printf "Extracted $counter profiles\n"
 }
 
 
 # User will determine the location of where these profiles will be
-function _getDest() {
+function _getUSBLocation() {
     DEST=""
 
     # Ask where the user wants to copy the profile to
@@ -67,10 +64,10 @@ function _getDest() {
             DEST="$dest"
         else
             printf "\e[31m\e[4m$dest\e[0m\e[31m DOES NOT exist\e[0m\n"
-            _getDest
+            _getUSBLocation
         fi
     else
-        _getDest
+        _getUSBLocation
     fi
 }
 
@@ -78,13 +75,16 @@ function _getDest() {
 # User selects between extraction or insertion
 function _selection
 {
-    read -p "Do you want to extract or insert? [e/i]: " choice
+    printf "Do you want to copy profile from...\n"
+    printf "1. Computer to USB\n"
+    printf "2. USB to Computer\n"
+    read -p "Select [1|2]: " choice
 
-    if   [ "$choice" == 'e' ]; then
-        _getDest
-        _extraction
-    elif [ "$choice" == 'i' ]; then
-        _insertion
+    if   [ $choice == 1 ]; then
+        _getUSBLocation
+        _computerToUSB
+    elif [ $choice == 2 ]; then
+        _USBtoComputer
     else
         _selection
     fi
